0714453782f183aa8671fb71d25e5fa86325bea4
DGDBAND <dgdband@hotmail.com>
2021-01-22 18:47:45 -0600

    Seguridad con Helmet y npm audit

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const helmet = require('helmet')
 const app = express();
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
@@ -28,6 +29,8 @@ app.use((request, response, next) =>{
     }
     next()
 })
+
+app.use(helmet())
 
 moviesApi(app)
 userMoviesApi(app)
